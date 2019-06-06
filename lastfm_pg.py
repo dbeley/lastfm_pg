207201baf279e780364a3737de0b8b37cb4ce971
dbeley <6568955+dbeley@users.noreply.github.com>
2019-06-06 17:34:00 +0200

    add mastodon bot

--- lastfm_pg.py
+++ lastfm_pg.py
@@ -9,36 +9,67 @@ import random
 import datetime
 import pylast
 import tweepy
+from mastodon import Mastodon
 from pathlib import Path
 from collections import defaultdict
 
 logger = logging.getLogger()
 logging.getLogger("pylast").setLevel(logging.WARNING)
 begin_time = datetime.datetime.now()
+
+config = configparser.ConfigParser()
+# same directory as the script
+config.read("config.ini")
+
 PLAYLIST_LENGTH = 10
 TWITTER_MAX_CHARACTERS = 280
+MASTODON_MAX_CHARACTERS = 500
 TIMEFRAME_VALUES = ["7day", "1month", "3month", "6month", "12month", "overall"]
+SUPPORTED_SOCIAL_MEDIA = ["twitter", "mastodon"]
 
 
 def twitterconnect():
-    config = configparser.ConfigParser()
-    # same directory as the script
-    config.read("config.ini")
-    ConsumerKey = config["twitter"]["ConsumerKey"]
-    SecretKey = config["twitter"]["SecretKey"]
-    AccessToken = config["twitter"]["AccessToken"]
-    AccessTokenSecret = config["twitter"]["AccessTokenSecret"]
+    consumer_key = config["twitter"]["consumer_key"]
+    secret_key = config["twitter"]["secret_key"]
+    access_token = config["twitter"]["access_token"]
+    access_token_secret = config["twitter"]["access_token_secret"]
 
-    auth = tweepy.OAuthHandler(ConsumerKey, SecretKey)
-    auth.set_access_token(AccessToken, AccessTokenSecret)
+    auth = tweepy.OAuthHandler(consumer_key, secret_key)
+    auth.set_access_token(access_token, access_token_secret)
     return tweepy.API(auth)
 
 
-def tweet_message(api, message):
-    api.update_status(status=message)
+def mastodonconnect():
+    if not Path("mastodon_clientcred.secret").is_file():
+        Mastodon.create_app(
+            "mastodon_bot_lastfm_cg",
+            api_base_url=config["mastodon"]["api_base_url"],
+            to_file="mastodon_clientcred.secret",
+        )
+
+    if not Path("mastodon_usercred.secret").is_file():
+        mastodon = Mastodon(
+            client_id="mastodon_clientcred.secret",
+            api_base_url=config["mastodon"]["api_base_url"],
+        )
+        mastodon.log_in(
+            config["mastodon"]["login_email"],
+            config["mastodon"]["password"],
+            to_file="mastodon_usercred.secret",
+        )
+
+    mastodon = Mastodon(
+        access_token="mastodon_usercred.secret",
+        api_base_url=config["mastodon"]["api_base_url"],
+    )
+    return mastodon
 
 
-def tweet_list_message(api, list_message):
+def tweet_list_message(api, list_message, social_media):
+    if social_media == "mastodon":
+        max_characters = MASTODON_MAX_CHARACTERS
+    elif social_media == "twitter":
+        max_characters = TWITTER_MAX_CHARACTERS
     iterator = iter(list_message)
     list_tweets_temp = []
     list_tweets = []
@@ -52,7 +83,7 @@ def tweet_list_message(api, list_message):
                 "Number of characters for tweet : %s.", sum_list_tweets_temp
             )
             nb_char = len(list_tweets_temp) + 6
-            if sum_list_tweets_temp > TWITTER_MAX_CHARACTERS - nb_char:
+            if sum_list_tweets_temp > max_characters - nb_char:
                 logger.debug(
                     "Max number of characters reached. Creating new tweet."
                 )
@@ -65,35 +96,50 @@ def tweet_list_message(api, list_message):
             logger.debug("Reached end of list_message.")
             break
     max_index = len(list_tweets)
+    logger.debug("max_index : %s", max_index)
     list_formatted_tweets = []
     for index, tweet in enumerate(list_tweets, 1):
         message = "\n".join(tweet)
-        list_formatted_tweets.append(f"{message} [{index}/{max_index}]")
+        if max_index > 1:
+            list_formatted_tweets.append(f"{message} [{index}/{max_index}]")
+        else:
+            list_formatted_tweets.append(f"{message}")
 
     for index, tweet in enumerate(list_formatted_tweets, 1):
         logger.debug("Posting tweet %s", index)
-        api.update_status(status=tweet)
+        if social_media == "mastodon":
+            api.status_post(tweet)
+        elif social_media == "twitter":
+            api.update_status(status=tweet)
 
 
 def lastfmconnect():
     config = configparser.ConfigParser()
     # same directory as the script
     config.read("config.ini")
-    API_KEY = config["lastfm"]["API_KEY"]
-    API_SECRET = config["lastfm"]["API_SECRET"]
+    api_key = config["lastfm"]["api_key"]
+    api_secret = config["lastfm"]["api_secret"]
 
-    network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET)
+    network = pylast.LastFMNetwork(api_key=api_key, api_secret=api_secret)
     return network
 
 
 def main():
     args = parse_args()
     network = lastfmconnect()
-    if args.no_upload_twitter:
-        logger.debug("No upload to twitter.")
+    social_media = args.social_media.lower()
+    if args.no_upload:
+        logger.debug("No upload mode activated.")
     else:
-        logger.debug("Upload to twitter.")
-        api = twitterconnect()
+        if social_media not in SUPPORTED_SOCIAL_MEDIA:
+            logger.error(
+                "Social media %s not supported. Exiting.", social_media
+            )
+            exit()
+        elif social_media == "twitter":
+            api = twitterconnect()
+        elif social_media == "mastodon":
+            api = mastodonconnect()
     if args.timeframe not in TIMEFRAME_VALUES:
         logger.error(
             "Incorrect value %s for timeframe. Accepted values : %s",
@@ -160,34 +206,22 @@ def main():
         start = begin_time - datetime.timedelta(days=begin_time.weekday())
         time = start + datetime.timedelta(days=6)
         title = f"My most played favorites tracks on #lastfm for the week of {time.strftime('%B %d %Y')}:"
-        export_filename = (
-            f"Exports/playlist_weekly_{time.strftime('%d-%m-%Y')}.txt"
-        )
+        export_filename = f"Exports/playlist_weekly_{time.strftime('%d-%m-%Y')}_{social_media}.txt"
     elif args.timeframe == "1month":
         title = f"My most played favorites tracks on #lastfm for {begin_time.strftime('%B %Y')}."
-        export_filename = (
-            f"Exports/playlist_monthly_{begin_time.strftime('%m-%Y')}.txt"
-        )
+        export_filename = f"Exports/playlist_monthly_{begin_time.strftime('%m-%Y')}_{social_media}.txt"
     elif args.timeframe == "3month":
         title = f"My most listened albums on #lastfm for the last 3 months."
-        export_filename = (
-            f"Exports/playlist_3months_{begin_time.strftime('%d-%m-%Y')}.txt"
-        )
+        export_filename = f"Exports/playlist_3months_{begin_time.strftime('%d-%m-%Y')}_{social_media}.txt"
     elif args.timeframe == "6month":
         title = f"My most listened albums on #lastfm for the last 6 months."
-        export_filename = (
-            f"Exports/playlist_6months_{begin_time.strftime('%d-%m-%Y')}.txt"
-        )
+        export_filename = f"Exports/playlist_6months_{begin_time.strftime('%d-%m-%Y')}_{social_media}.txt"
     elif args.timeframe == "12month":
         title = f"My most listened albums on #lastfm for the last 12 months."
-        export_filename = (
-            f"Exports/playlist_12months_{begin_time.strftime('%d-%m-%Y')}.txt"
-        )
+        export_filename = f"Exports/playlist_12months_{begin_time.strftime('%d-%m-%Y')}_{social_media}.txt"
     elif args.timeframe == "overall":
         title = f"My most listened albums on #lastfm ever."
-        export_filename = (
-            f"Exports/playlist_overall_{begin_time.strftime('%d-%m-%Y')}.txt"
-        )
+        export_filename = f"Exports/playlist_overall_{begin_time.strftime('%d-%m-%Y')}_{social_media}.txt"
 
     logger.debug(
         "timeframe : %s, title : %s, export_filename : %s",
@@ -201,7 +235,7 @@ def main():
         for index, track in reversed(list(enumerate(playlist_tracks, 1))):
             f.write(f"{str(index).zfill(2)}: {track.artist} - {track.title}\n")
 
-    # Creating twitter message list
+    # Creating message list
     # headers_message = [title, "Made with https://github.com/dbeley/lastfm_pg"]
     headers_message = [title, "#lastfm"]
     list_message = []
@@ -216,11 +250,9 @@ def main():
     list_message.insert(0, headers_message[0])
     list_message.append(headers_message[1])
 
-    # Upload to twitter
-    if args.no_upload_twitter:
-        logger.info("No posting mode activated.")
-    else:
-        tweet_list_message(api, list_message)
+    # Upload to social media
+    if not args.no_upload:
+        tweet_list_message(api, list_message, social_media)
 
 
 def parse_args():
@@ -239,9 +271,9 @@ def parse_args():
     )
     parser.add_argument("--username", "-u", help="Lastfm username", type=str)
     parser.add_argument(
-        "--no_upload_twitter",
-        help="Disable the twitter upload. Use it for debugging",
-        dest="no_upload_twitter",
+        "--no_upload",
+        help="Disable the social media upload. Use it for debugging",
+        dest="no_upload",
         action="store_true",
     )
     parser.add_argument(
@@ -253,9 +285,15 @@ def parse_args():
         type=str,
         default="7day",
     )
-    parser.set_defaults(no_upload_twitter=False)
+    parser.add_argument(
+        "--social-media",
+        "-s",
+        help="Social media where the playlist will be posted (twitter or mastodon. Default = twitter).",
+        type=str,
+        default="twitter",
+    )
+    parser.set_defaults(no_upload=False)
     args = parser.parse_args()
-
     logging.basicConfig(level=args.loglevel, format=format)
     return args
 
